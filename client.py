9f87d827d74edfda58d7836b2b8f54e5ca4bff3b
Lucía Zamora Gómez <lucia.zamora1@alumni.esade.edu>
2026-05-09 16:50:46 +0200

    final revised version

--- client.py
+++ client.py
@@ -1,11 +1,8 @@
 from user import User
 from vehicle import Vehicle
-
 from exceptions import VehicleNotFoundError, ClientAlreadyHasVehicleError
 
-
 class Client(User):
-
     def __init__(self, name, date_of_birth, user_id):
         super().__init__(name, date_of_birth, user_id)
         self.__vehicles = []
@@ -15,13 +12,10 @@ class Client(User):
         return list(self.__vehicles)
 
     def add_vehicle(self, vehicle):
-        if not isinstance(vehicle, Vehicle):
+        if not isinstance(vehicle, Vehicle): #Checks if vehicle is a Vehicle object 
             raise TypeError("Only Vehicle objects can be added.")
-
         if vehicle in self.__vehicles:
-            raise ClientAlreadyHasVehicleError(
-                f"{vehicle.license_plate} already belongs to this client."
-            )
+            raise ClientAlreadyHasVehicleError(f"{vehicle.license_plate} already belongs to this client.")
 
         self.__vehicles.append(vehicle)
 
@@ -30,15 +24,13 @@ class Client(User):
         self.__vehicles.remove(vehicle)
 
     def _find_vehicle(self, license_plate):
-        plate = license_plate.upper().strip()
+        plate = license_plate.upper().strip() #formatting
 
         for vehicle in self.__vehicles:
             if vehicle.license_plate == plate:
                 return vehicle
-
-        raise VehicleNotFoundError(
-            f"Vehicle {license_plate} not found."
-        )
+        else:
+            raise VehicleNotFoundError(f"Vehicle {license_plate} not found.")
 
     def update_vehicle_mileage(self, license_plate, new_mileage):
         vehicle = self._find_vehicle(license_plate)
@@ -57,16 +49,13 @@ class Client(User):
         return vehicle.next_maintenance()
 
     def get_info(self):
-        plates = [v.license_plate for v in self.__vehicles]
+        plates = [v.license_plate for v in self.__vehicles] #creates a list containing the license plates of all vehicles in self.__vehicles.
 
         return (
             f"Client | {self.name} | ID: {self.user_id} "
-            f"| Vehicles: {plates}"
-        )
+            f"| Vehicles: {plates}")
 
-    def to_csv_row(self):
-        row = super().to_csv_row()
-        row["vehicles"] = ";".join(
-            v.license_plate for v in self.__vehicles
-        )
+    def to_csv_row(self): #creates a method that converts the object into a dictionary
+        row = super().to_csv_row() #calls the parent class (User) to_csv_row() method and gets its dictionary --> so that the child calss can add information
+        row["vehicles"] = ";".join(v.license_plate for v in self.__vehicles) #creates one string with all the vehicle plates separated by ;
         return row
